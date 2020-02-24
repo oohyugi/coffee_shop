6b7f149c919b76ea1cabff34acbfa095df23369e
oohyugi <yogi.stickearn@gmail.com>
2020-02-24 14:11:16 +0700

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
-import 'package:coffee_shop/feature/order/order_page.dart';
+import 'package:coffee_shop/main_page.dart';
 import 'package:flutter/material.dart';
-
-import 'feature/order/order_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          accentColor: Colors.pink,
-          primaryColor: Colors.pink,
-          buttonColor: Colors.pink),
-      home: OrderPage(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            accentColor: Colors.pink,
+            primaryColor: Colors.pink,
+            buttonColor: Colors.pink),
+        home: MainPage());
   }
 }
---
+++ lib/main_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+import 'feature/order/order_page.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() {
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  List<NavigationItem> items = [
+    NavigationItem(Icon(Icons.home), Text("Home")),
+    NavigationItem(Icon(Icons.assignment), Text("Menu")),
+    NavigationItem(Icon(Icons.search), Text("Search")),
+    NavigationItem(Icon(Icons.person), Text("Account")),
+  ];
+  int selectedIndex = 0;
+
+  List<Widget> listBodyChildreen = [
+    Center(
+      child: Text("Home"),
+    ),
+    OrderPage(),
+    Center(
+      child: Text("Search"),
+    ),
+    Center(
+      child: Text("Account"),
+    ),
+  ];
+
+  Widget _buildItem(NavigationItem item, bool isSelected) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 260),
+      width: isSelected ? 125 : 50,
+      height: 50,
+      padding: isSelected ? EdgeInsets.only(left: 8, right: 8) : null,
+      decoration: BoxDecoration(
+          color: isSelected ? Theme.of(context).accentColor : null,
+          borderRadius: BorderRadius.circular(24)),
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              IconTheme(
+                data: IconThemeData(
+                    size: 24,
+                    color: isSelected ? Colors.white : Colors.black54),
+                child: item.icon,
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: isSelected
+                      ? DefaultTextStyle.merge(
+                          style: TextStyle(color: Colors.white),
+                          child: item.title)
+                      : Container()),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 56,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [BoxShadow(blurRadius: 4, color: Colors.black12)]),
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, top: 4, bottom: 4, right: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: items.map((item) {
+              var itemIndex = items.indexOf(item);
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedIndex = itemIndex;
+                  });
+                },
+                child: _buildItem(item, selectedIndex == itemIndex),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+      body: listBodyChildreen[selectedIndex],
+    );
+  }
+}
+
+class NavigationItem {
+  final Icon icon;
+  final Text title;
+
+  NavigationItem(this.icon, this.title);
+}
